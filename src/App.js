38c5ef4d6d1047ab3348528d04f60f4de1cbb6de
abhishek <abhishekmazumder69@gmail.com>
2021-06-28 15:11:15 +0530

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { css } from '@emotion/react';
+import PropagateLoader from 'react-spinners/PropagateLoader';
+
+import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        logo
-      </header>
-    </div>
-  );
+	const [loading, setLoading] = useState(false);
+	const override = css`
+		display: block;
+		border-color: red;
+		margin-top: 20%;
+	`;
+
+	useEffect(() => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+		}, 5000);
+	}, []);
+
+	return (
+		<div className="App">
+			{loading ? (
+				<PropagateLoader
+					loading={loading}
+					color={'#3d2514'}
+					css={override}
+					size={40}
+				/>
+			) : (
+				<>
+					<Navbar />
+				</>
+			)}
+		</div>
+	);
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link} from "react-scroll"
+import logo from "../images/logo1.png"
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const changeBackground= ()=>{
+    if(window.scrollY >= 50){
+      setNav(true)
+    }else{
+      setNav(false)
+    }
+  }
+
+  window.addEventListener("scroll", changeBackground)
+
   return (
-    <div>
-      
-    </div>
+    <nav className={nav ? "nav active" : "nav"}>
+      <Link to="main" className="logo">
+        <img src={logo} alt="burgeract-house logo"/>
+      </Link>
+
+      <input className="menu-btn" type="checkbox" id="menu-btn" />
+      <label className="menu-icon" for="menu-btn">
+        <span className="nav-icon">
+
+        </span>
+      </label>
+
+      <ul className="menu">
+        <li><Link to="#">Header</Link></li>
+        <li><Link to="#">Menus</Link></li>
+        <li><Link to="#">About</Link></li>
+        <li><Link to="#">Contact Us</Link></li>
+      </ul>
+    </nav>
   )
 }
 
